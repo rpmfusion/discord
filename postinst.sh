2837b48bbcd217093a45188047be45dd8ef15f23
ValentinTainon <valentin.tainon@outlook.fr>
2024-04-09 19:42:13 +0200

    bugfix(discord pop-up): editing postinst.sh script present in the rpm package to add a patch for resolve the check update pop-up issue when starting the application

---
+++ postinst.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/sh
+
+# os.tmpdir from node.js
+for OS_TMPDIR in "$TMPDIR" "$TMP" "$TEMP" /tmp
+do
+  test -n "$OS_TMPDIR" && break
+done
+
+# kill any currently running Discord
+if pgrep Discord ; then
+  pkill Discord
+  sleep 1
+  pkill -9 Discord
+fi
+
+# This is probably just paranoia, but some people claim that clearing out
+# cache and/or the sock file fixes bugs for them, so here we go
+for DIR in /home/* ; do
+  rm -rf "$DIR/.config/discord/Cache"
+  rm -rf "$DIR/.config/discord/GPUCache"
+
+  # A previous bug made some files in this folder owned by root
+  # and discord will hang if those files are present
+  SETTINGS_FILE="$DIR/.config/discord/Crashpad/settings.dat"
+  if [ -f "$SETTINGS_FILE" ]; then
+    OWNER=$(stat -c "%U" "$SETTINGS_FILE")
+    if [ "$OWNER" = "root" ]; then
+      rm -rf "$DIR/.config/discord/Crashpad"
+    fi
+  fi
+done
+rm -f "$OS_TMPDIR/discord.sock"
+
+### START OF PATCH (SKIP UPDATE POP-UP) ###
+# Check if discord user configuration directory exist
+DISCORD_USER_CONFIG_DIR="/home/$(logname)/.config/discord"
+DISCORD_USER_SETTINGS_JSON_FILE="/home/$(logname)/.config/discord/settings.json"
+if [ ! -d "$DISCORD_USER_CONFIG_DIR" ]; then
+    mkdir -m 700 "$DISCORD_USER_CONFIG_DIR"
+    chown $(logname):$(logname) "$DISCORD_USER_CONFIG_DIR"
+fi
+
+# If 'settings.json' file doesn't exist: create file and set "SKIP_HOST_UPDATE" to true
+if [ ! -f "$DISCORD_USER_SETTINGS_JSON_FILE" ]; then
+    echo '{
+    "SKIP_HOST_UPDATE": true
+}' >"$DISCORD_USER_SETTINGS_JSON_FILE"
+    chmod 644 "$DISCORD_USER_SETTINGS_JSON_FILE"
+    chown $(logname):$(logname) "$DISCORD_USER_SETTINGS_JSON_FILE"
+# If 'settings.json' file exist and if "SKIP_HOST_UPDATE" doesn't set : set it to true
+elif ! grep "SKIP_HOST_UPDATE" "$DISCORD_USER_SETTINGS_JSON_FILE"; then
+    # Get 'settings.json' content without final closing brace, add "SKIP_HOST_UPDATE" 
+    # line with final closing brace, and save it to the file
+    echo "$(grep -v '^}' $DISCORD_USER_SETTINGS_JSON_FILE),
+	\"SKIP_HOST_UPDATE\": true
+}" >"$DISCORD_USER_SETTINGS_JSON_FILE"
+fi
+### END OF PATCH (SKIP UPDATE POP-UP) ###
