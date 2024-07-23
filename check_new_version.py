b4f0690a83c052a43d2ff03d7c566bd3d1fa5484
Sérgio M. Basto <sergio@serjux.com>
2024-07-23 14:16:44 +0100

    Update to 0.0.61

--- check_new_version.py
+++ check_new_version.py
@@ -40,11 +40,12 @@ if spec != spec3:
         print('error running runme')
 
     print("New version available!")
+    print('spectool -g discord.spec')
     print('rfpkg mockbuild -N --default-mock-resultdir --root fedora-39-x86_64-rpmfusion_nonfree')
 else:
     print("Already updated !")
+    print('spectool -g discord.spec')
 
-print('spectool -g discord.spec')
 print("rfpkg new-sources $(spectool -l --sources discord.spec | grep https | sed 's/.*: //;s#.*/##')")
 print('rfpkg ci -c && git show && echo Press enter to push and build; read dummy; rfpkg push && rfpkg build --nowait')
 print('git checkout f40 && git merge master && git push && rfpkg build --nowait; git checkout master')
